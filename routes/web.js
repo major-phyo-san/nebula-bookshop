ea3a9127c44ba1a726f2714433853a8984f7837a
major-phyo-san <phyojupiter9@gamil.com>
2020-12-05 11:40:29 +0630

    subcategory routes

--- routes/web.js
+++ routes/web.js
@@ -2,23 +2,36 @@ var express = require('express');
 var router = express.Router();
 var envs = require('../config/server-env');
 
-var CategoresController = require('../app/controllers/CategoriesController');
+var CategoriesController = require('../app/controllers/CategoriesController');
+var SubcategoriesController = require('../app/controllers/SubcategoriesController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {appName: envs.NODE_NAME});
 });
 
-/* GET categories listing. */
-router.get('/categories', CategoresController.getAllCategories);
+/* Categories routes */
+router.get('/categories', CategoriesController.getAllCategories);
 
-router.get('/categories/new', CategoresController.showCreateForm);
-router.post('/categories', CategoresController.addNewCategory);
+router.get('/categories/new', CategoriesController.showCreateForm);
+router.post('/categories', CategoriesController.addNewCategory);
 
-router.get('/categories/:catId/edit', CategoresController.showEditForm);
-router.put('/categories/:catId', CategoresController.editCategory);
+router.get('/categories/:catId/edit', CategoriesController.showEditForm);
+router.put('/categories/:catId', CategoriesController.editCategory);
 
-router.get('/categories/:catId/delete', CategoresController.showDeleteForm);
-router.delete('/categories/:catId', CategoresController.deleteCategory);
+router.get('/categories/:catId/delete', CategoriesController.showDeleteForm);
+router.delete('/categories/:catId', CategoriesController.deleteCategory);
+
+/* Categories routes */
+router.get('/categories/:catId/subcategories', SubcategoriesController.getAllSubcategories);
+
+router.get('/subcategories/new', SubcategoriesController.showCreateForm);
+router.post('/subcategories', SubcategoriesController.addNewSubcategory);
+
+router.get('/subcategories/:subcatId/edit', SubcategoriesController.showEditForm);
+router.put('/subcategories/:subcatId', SubcategoriesController.editSubcategory);
+
+router.get('/subcategories/:subcatId/delete', SubcategoriesController.showDeleteForm);
+router.delete('/subcategories/:subcatId', SubcategoriesController.deleteSubcategory);
 
 module.exports = router;
