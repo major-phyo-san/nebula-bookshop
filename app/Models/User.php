20024715e74e324a451e8796f57fff6987195561
Phyo Kyaw San <phyojupiter9@gmail.com>
2022-03-08 18:07:25 +0630

    User model fixed for phone number auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        'phone_number',
         'password',
     ];
 
@@ -39,6 +39,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'phone_number_verified_at' => 'datetime',
     ];
 }
